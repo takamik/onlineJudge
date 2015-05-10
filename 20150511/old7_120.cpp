7777706611c5db82d2795b1253fb76d29e2f2b5a
takamik <takami.kaz@gmail.com>
2015-05-10 22:38:10 +0000

    120 commit

--- 20150511/old7_120.cpp
+++ 20150511/old7_120.cpp
@@ -12,7 +12,7 @@ vector<int> createNumList() {
   int num;
 
   int i = 0;
-  while (i < maxlen_num_list) {
+  while (i < maxlen_num_list+1) {
     if(cin.fail()) {
       cin.clear();
       cin.ignore();
@@ -34,17 +34,28 @@ break;
 int sortNumList(vector<int> num_list) {
   vector<int>::iterator max = max_element(num_list.begin(), num_list.end());
   int num_length = num_list.size();
+//  int cnt = num_length;
   int cnt = 1;
 
+int i=0;
+
+//  while(cnt > 0){
   while(cnt < num_length+1){
     max = max_element(num_list.begin(), num_list.end());
     
     if(*max == num_list.back()){
       //no reverse
       num_list.pop_back();
+//      --cnt;
       ++cnt;
     }
     else if(*max == num_list.front()){
+/*      if(cnt == num_length){
+        cout << "*" << 1 << "*" << ' ';
+      }
+      else {
+      }
+*/
       cout << cnt << ' ';
       reverse(num_list.begin(), num_list.end());
     }
@@ -52,6 +63,9 @@ int sortNumList(vector<int> num_list) {
       cout << (num_length - distance(num_list.begin(), max)) << ' ';
       reverse(num_list.begin(), max+1);
     }
+
+if(i < 10) { ++i; } else { break; }
+
   }
 
   cout << 0 << endl;
