70a250ee2e7cfbabd47b32a9f9c618c7d3deb672
takamik <takami.kaz@gmail.com>
2015-06-14 08:02:17 +0000

    20150614 commited

---
+++ 20150608/old_10010.cpp
@@ -0,0 +1,27 @@
+// Problem 10010
+// uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&category=12&page=show_problem&problem=949
+#include <iostream>
+//#include <sstream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+vector createCharMap(const int& cnt, const unsigned long& palindrome) {
+  cout << cnt << " " << palindrome << endl;
+}
+
+
+bool searchChar() {
+  return false;
+}
+
+void printAnswer(const int& cnt, const unsigned long& palindrome) {
+  cout << cnt << " " << palindrome << endl;
+}
+
+int main() {
+  }
+
+  return 0;
+}
